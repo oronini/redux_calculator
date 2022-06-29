c473bbcbe52fd0dcf818c3e9bb60b0dccea61da0
mikami koji <wmd147wmd@gmail.com>
2022-06-30 08:13:23 +0900

    完成

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App";
+import reducer from "./redux/reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
